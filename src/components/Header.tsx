e53222b2ac0fe9a14c8d1b487d9d55f85ca3baf0
Olaide-Ismaila <olaideolutunji@gmail.com>
2025-12-23 20:17:27 +0100

    made and style the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,25 @@
+import { FaArrowRight, FaCartPlus } from 'react-icons/fa';
+import logo from '../images/divine-logo.jpg';
+
 function Header() {
   return (
-    <div>
-  <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  </div>
+    <div className='lg:flex justify-between bg-green-700 text-white py-2 px-4 sm:block sm:m-auto sm:align-middle'>
+      <div className='flex justify-center p-2 lg:w-[22%] font-semibold sm:w-full'>
+        <img src={logo} alt="Logo" className='w-10 h-10 mr-4' />
+        <h1 className="text-2xl">
+         DivineProwessMusic 
+        </h1>
+      </div>
+      <div className='flex justify-center lg:w-[60%] sm:w-full'>
+        <input type="text" placeholder='Search' className='bg-white text-black w-[90%] px-4 py-2 rounded-l-sm' />
+        <FaArrowRight className='my-auto text-black bg-green-300 size-14 p-4 rounded-r-sm' />
+      </div>
+      <div className='flex justify-center p-2 lg:w-[18%] font-semibol sm:w-full'>
+        <span className='mr-10 text-2xl'>Orders</span>
+        <FaCartPlus className='text-4xl mr-2' />
+        <span className='text-2xl'>Cart</span>
+      </div>
+    </div>
   );
 }
 
